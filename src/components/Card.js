7d2817a8dd5d47a10e85f54d863b482ff17c5e7e
Veronica isla <veroislagleaniz@gmail.com>
2022-10-21 11:51:49 +0200

    added variables colors and change palette cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,18 +10,21 @@ function Card(props) {
         alt={props.character.name}
         title={props.character.name}
       />{' '}
-      <h4 className="card__name">{props.character.name}</h4>
-      <p className="card__location">{props.character.location.name}</p>
-      <div
-        className={
-          props.character.status === 'Dead'
-            ? 'card__status dead'
-            : '' || props.character.status === 'unknown'
-            ? 'card__status unknown'
-            : 'card__status'
-        }
-      >
-        {props.character.status}
+      <div className="card__info">
+        <h4 className="card__name">{props.character.name}</h4>
+        <p className="card__location">Localización:</p>
+        <p className="card__locationName">{props.character.location.name}</p>
+        <div
+          className={
+            props.character.status === 'Dead'
+              ? 'card__status card__status--dead'
+              : '' || props.character.status === 'unknown'
+              ? 'card__status card__status--unknown'
+              : 'card__status'
+          }
+        >
+          {props.character.status}
+        </div>
       </div>
     </div>
   );
