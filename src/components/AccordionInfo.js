0994fa16a2fb125e2a7784ee6c8a13da7855b902
Veronica isla <veroislagleaniz@gmail.com>
2022-11-02 12:39:49 +0100

    added aniamtion to accordion

--- src/components/AccordionInfo.js
+++ src/components/AccordionInfo.js
@@ -1,28 +1,60 @@
-import Accordion from 'react-bootstrap/Accordion';
-import React from 'react';
+import React, { useState } from 'react';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/components/Accordion.scss';
 
 function AccordionInfo(props) {
-  return (
-    <section className="accordion">
-      {' '}
-      <Accordion>
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>About</Accordion.Header>
-          <Accordion.Body>fafs</Accordion.Body>
-        </Accordion.Item>
+  const [selected, setsSelected] = useState(null);
 
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Characters</Accordion.Header>
-          <Accordion.Body>fdasf</Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
+  const toggle = (item) => {
+    if (selected === item) {
+      return setsSelected(null);
+    }
+
+    setsSelected(item);
+  };
+  return (
+    <section className="accordion__wrapper">
+      <div className="accordion__accordion">
+        {data.map((item, index) => (
+          <div className="accordion__item">
+            <div
+              className="accordion__titleAndContent"
+              onClick={() => toggle(index)}
+            >
+              <h2 className="accordion__title">{item.question}</h2>
+              <span>{selected === index ? '-' : '+'}</span>
+            </div>
+            <div
+              className={
+                selected === index
+                  ? 'accordion__content show'
+                  : 'accordion__content'
+              }
+            >
+              {item.answer}
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
 
+const data = [
+  {
+    question: 'Question 1',
+    answer:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est, ipsam cum ',
+  },
+  {
+    question: 'Question 2',
+    answer:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est, ipsam cum ',
+  },
+  {
+    question: 'Question 3',
+    answer:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est, ipsam cum ',
+  },
+];
 export default AccordionInfo;
-
-// --> https://react-bootstrap.github.io/components/accordion/
